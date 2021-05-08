7c56aa340ffb2242e58c3bb94d976576e6a82e50
Remcovdvoort <remcovandervoort00@gmail.com>
2021-05-08 17:04:31 +0200

    Begin modified condition/decision coverage

---
+++ src/test/ModifiedTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ModifiedTest {
+    @Test
+    public void kloppendePlanning() {
+        Planning planning = new Planning (true, 1, true);
+        assertFalse(planning.kloppendePlanning(false, -10, true));
+        assertTrue(planning.kloppendePlanning(true, 20, false));
+        assertTrue(planning.kloppendePlanning(true, -15, true));
+        assertFalse(planning.kloppendePlanning(false, -19, false));
+    }
+}
